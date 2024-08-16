e453cba2cf0a1c48108f82e61208779d011b3f3b
prathamisonline <prathamsrivastavas@gmail.com>
2024-08-16 19:29:48 +0530

    added online offline indicator

--- frontend/src/views/chats/components/messages/components/MessageTopBar.jsx
+++ frontend/src/views/chats/components/messages/components/MessageTopBar.jsx
@@ -1,30 +1,32 @@
-import { useSearchParams } from "react-router-dom";
 import UseChatApi from "../../../../../store/chat/useChatApi";
 import { useEffect } from "react";
+import { useWebSocket } from "../../../../../store/Websocket/UseWebsocket";
+import { useRecoilValue } from "recoil";
+import { SelectedUserState } from "../../../../../states/theme";
 
 const MessageTopBar = () => {
-  const [searchParams] = useSearchParams();
-  const { getChatUsers, chatUsers } = UseChatApi();
+  const { getChatUsers } = UseChatApi();
+  const { onlineUsers } = useWebSocket();
+
+  const selectedUser = useRecoilValue(SelectedUserState);
+
+  const isOnline = onlineUsers.includes(selectedUser._id);
 
   useEffect(() => {
     getChatUsers();
   }, [getChatUsers]);
 
-  const findUser = chatUsers.find((user) => {
-    return user._id === searchParams.get("id");
-  });
-
   return (
     <div className=" flex justify-start items-center h-[58px] p-4">
       <div className="flex justify-start items-center gap-4">
-        <div className="avatar online">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-10 rounded-full">
-            <img src={findUser?.profilePic} />
+            <img src={selectedUser?.profilePic} />
           </div>
         </div>
         <div>
           <div className="font-semibold dark:text-red-600">
-            {findUser?.fullname}
+            {selectedUser?.fullname}
           </div>
           <div className="text-[12px] text-gray-500">Last seen 5 min ago</div>
         </div>
