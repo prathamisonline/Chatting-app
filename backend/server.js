89fe86e07071aee88e0dbd887961d12a91b0d836
prathamisonline <prathamsrivastavas@gmail.com>
2024-08-16 12:05:46 +0530

    env changes

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,30 @@
+import path from "path"
 import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import connectToMongoDB from "./db/connectToMongoDB.js"
+
 import authRoutes from "./router/authRoutes.js"
 import userRoutes from "./router/useRoutes.js"
 import messageRoutes from "./router/messageRoutes.js"
-import dotenv from "dotenv";
+
 import { app, server } from "./socket/socket.js";
+import connectToMongoDB from "./db/connectToMongoDB.js"
 
 dotenv.config();
+
+const __dirname = path.resolve()
+
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
-// const io = new Server(server);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
-app.get("/", (req, res) => {
-    res.send("hello worlddfs")
-})
 
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(4000, async () => {
     await connectToMongoDB();
